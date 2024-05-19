aa047ad7592ec9c8f35b2796647ac84160708b85
favopg <favoriteigo0904@gmail.com>
2024-05-19 21:36:58 +0900

    'マニフェスト定義追加'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,3 +47,11 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "kotlin_log.AppKt"
+        )
+    }
+}
